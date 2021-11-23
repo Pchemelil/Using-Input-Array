192794023cbb21cba24cb4b9367bc56677eb7b26
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-11-23 20:44:14 +0300

    Add files via upload

---
+++ UserInputArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num[5];
+
+cout<<"Please input 5 integers:\n";
+
+cin>> num[1];
+cin>> num[2];
+cin>> num[3];
+cin>> num[4];
+cin>> num[5];
+
+cout<<"\nHere are your integers: \n";
+
+cout<< num[1]<<", ";
+cout<< num[2]<<", ";
+cout<< num[3]<<", ";
+cout<< num[4]<<", ";
+cout<< num[8];
+
+return 0;
+}
